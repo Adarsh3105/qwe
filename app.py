df61f9201bb3766876ae71140a5153ae22d560c3
Adarsh3105 <56245889+Adarsh3105@users.noreply.github.com>
2020-10-10 14:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__);
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    res = query + " " + time.ctime()
+    if query is "Hello":
+        res="Hi"
+    else:
+        res="I don't know"
+    #res = query + " " + time.ctime()
     return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
