f664245594fac5e3bb0c1caa76c6d208e5a278d9
Adarsh3105 <56245889+Adarsh3105@users.noreply.github.com>
2020-10-10 14:57:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify,request
 import time
-app = Flask(__name__);
-@app.route("/bot", methods=["POST"])
-def response():
-    query = dict(request.form)['query']
+
+def check(query):
     if query=='Hello'or query=='Hi':
         res="Hey there!"
     if query=='How are you?':
@@ -12,7 +10,12 @@ def response():
         res="Coronaviruses are a large family of viruses which may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS). The most recently discovered coronavirus causes coronavirus disease COVID-19."
     if query=='What is covid-19?' or query=='Tell me about covid-19':
         res="COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019"
-
+    return res
+app = Flask(__name__);
+@app.route("/bot", methods=["POST"])
+def response():
+    query = dict(request.form)['query']
+    res=check(query)
     #res = query + " " + time.ctime()
     return jsonify({"response" : res})
 if __name__=="__main__":
