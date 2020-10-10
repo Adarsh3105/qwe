291f9c93378a52548416e04fe6eec3ab6c6bed44
Adarsh3105 <56245889+Adarsh3105@users.noreply.github.com>
2020-10-10 15:02:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify,request
 import time
 
 def check(query):
-    if query=='How are you?':
+    if query=="How are you?":
         res="I'm good"
     if query=='Hello'or query=='Hi':
         res="Hey there!"
-    if query=='What is coronavirus?' or query=='What is a coronavirus?' or query=='What is corona?':
+    if query=="What is coronavirus?" or query=="What is a coronavirus?" or query=="What is corona?":
         res="Coronaviruses are a large family of viruses which may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS). The most recently discovered coronavirus causes coronavirus disease COVID-19."
-    if query=='What is covid-19?' or query=='Tell me about covid-19':
+    if query=="What is covid-19?" or query=="Tell me about covid-19":
         res="COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019"
     return res
 app = Flask(__name__);
